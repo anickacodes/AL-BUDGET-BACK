065d499d9bf5a7b4e8d0633d7a3cc3a78b059803
anickacodes <anickalewis@pursuit.org>
2023-10-23 12:54:39 -0400

    put functionality

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const cors = require("cors");
-const transactionsController = require('./controllers/transactionsController')
+const transactionsController = require("./controllers/transactionsController");
 
-app.use(express.json())
-app.use(cors())
+app.use(cors());
 
-app.use('/transactions', transactionsController)
+app.use(express.json());
 
-module.exports = app
+app.use("/transactions", transactionsController);
+
+module.exports = app;
--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -8,7 +8,7 @@ transactionRouter.use(express.json());
 transactionRouter.get("/", (req, res, next) => {
   try {
     if (transactionArray && transactionArray.length > 0) {
-      res.send(transactionArray);
+      res.status(200).send(transactionArray);
     } else {
       res.status(404).send({ message: "Transactions are Not Found" });
     }
@@ -17,36 +17,64 @@ transactionRouter.get("/", (req, res, next) => {
   }
 });
 
+// READ Transactions SHOW
 
-// READ Transactions SHOW 
-
-transactionRouter.get('/:id', (req, res, next) => {
-
-try{
-    const {id }= req.params
-    const transactionObj = transactionArray.find(transaction => transaction.id === parseInt(id))
+transactionRouter.get("/:id", (req, res, next) => {
+  try {
+    const id = req.params;
+    const transactionObj = transactionArray.find(
+      (transaction) => transaction.id === parseInt(id)
+    );
     // parseInt turns a  string into a valid integer
-    if(transactionObj) {
-        res.send(transactionObj)
+    if (transactionObj) {
+      res.status(200).send(transactionObj);
+    } else {
+      res.status(404).send({ message: "cound not find transaction" });
     }
-    else {
-        res.status(404).send({message: "cound not find transaction"})
+  } catch (error) {
+    next(error);
+  }
+});
+
+transactionRouter.post("/", (req, res, next) => {
+  try {
+    const transactionBody = req.body;
+    if (transactionBody) {
+      transactionArray.push(transactionBody);
+      res.status(201).send(transactionArray);
+    } else {
+      res.status(404).send({ message: "Transaction was not created" });
     }
-}
-catch(error) {
-next(error)
-}
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
+transactionRouter.put("/:id", (req, res, next) => {
+  try {
+    const transactionId = parseInt(req.params.id);
+    const transactionToUpdate = req.body;
+    const transationIndex = transactionArray.findIndex(
+      (el) => el.id === transactionId
+    );
+    if (transationIndex === -1) {
+      res.status(404).send({ message: "Transaction to update not found" });
+    }
+// data transaction id match 
+    const currentTransaction = transactionArray[transationIndex];
+    // loop through requested transactions Array
+    for (let key in transactionToUpdate) {
+      if (currentTransaction.hasOwnProperty([key])) {
+        currentTransaction[key] = transactionToUpdate[key];
+        // set this to then equal req.body key
+      }
+    }
+    // update transactionArray to now have new transaction
+    transactionArray[transationIndex] = currentTransaction;
+    res.status(201).send(currentTransaction)
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = transactionRouter;
